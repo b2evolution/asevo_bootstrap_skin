43f29eb82509ab26ae664a20b0c823c4664f0bee
yurabakhtin <yurybakh@gmail.com>
2017-11-09 17:33:28 +0300

    Update version to 7

--- _skin.class.php
+++ _skin.class.php
@@ -21,7 +21,7 @@ class asevo_bootstrap_Skin extends Skin
 	 * Skin version
 	 * @var string
 	 */
-	var $version = '1.0.0';
+	var $version = '7.0.0';
 
 	/**
 	 * Do we want to use style.min.css instead of style.css ?
